5b227a96537e5f37ebcc14bf3cebc0b615b9fb1a
Martin Ždila <m.zdila@gmail.com>
2025-03-25 07:27:40 +0100

    reordered options

--- laz2dem/README.md
+++ laz2dem/README.md
@@ -1,3 +1,51 @@
+Usage:
+
+```
+Usage: laz2dem [OPTIONS] --bbox <BBOX> --zoom-level <ZOOM_LEVEL> --shadings <SHADINGS> <--laz-tile-db <LAZ_TILE_DB>|--laz-index-db <LAZ_INDEX_DB>> <OUTPUT>
+
+Arguments:
+  <OUTPUT>  Output mbtiles file
+
+Options:
+      --laz-tile-db <LAZ_TILE_DB>
+          Source as LAZ tile DB
+      --laz-index-db <LAZ_INDEX_DB>
+          Source as LAZ index DB referring *.laz files
+      --bbox <BBOX>
+          EPSG:3857 bounding box to render
+      --source-projection <SOURCE_PROJECTION>
+          Projection of points if reading from *.laz; default is EPSG:3857
+      --zoom-level <ZOOM_LEVEL>
+          Max zoom level of tiles to generate
+      --unit-zoom-level <UNIT_ZOOM_LEVEL>
+          If LAZ tile DB is used then use value of `--zoom-level` argument of `laztile` If LAZ index is used then use zoom level to determine size of tile to process at once [default: 16]
+      --shadings <SHADINGS>
+          Shadings; `+` separated componets of shading. Shading component is <method>,method_param1[,method_param2...].
+          ‎
+          Methods:
+          - `oblique` - params: azimuth in degrees, alitutde in degrees
+          - `igor` - params: azimuth in degrees
+          - `slope` - params: alitutde in degrees
+      --contrast <CONTRAST>
+          Increase (> 1.0) or decrease (< 1.0) contrast of the shading. Use value higher than 0.0 [default: 1]
+      --brightness <BRIGHTNESS>
+          Increase (> 0.0) or decrease (< 0.0) brightness of the shading. Use value between -1.0 and 1.0 [default: 0]
+      --z-factor <Z_FACTOR>
+          Z-factor [default: 1]
+      --tile-size <TILE_SIZE>
+          Tile size [default: 256]
+      --buffer <BUFFER>
+          Buffer size in pixels to prevent artifacts at tieledges [default: 40]
+      --format <FORMAT>
+          Tile image format. For alpha (transparency) support use `png` [default: jpeg] [possible values: jpeg, png]
+      --jpeg-quality <JPEG_QUALITY>
+          Quality from 0 to 100 when writing to JPEG [default: 80]
+      --background-color <BACKGROUND_COLOR>
+          Background color when writing to JPEG as it does not support alpha [default: FFFFFF]
+  -h, --help
+          Print help
+```
+
 Example:
 
 ```sh
--- laz2dem/src/options.rs
+++ laz2dem/src/options.rs
@@ -14,9 +14,30 @@ pub struct Options {
     /// Output mbtiles file
     pub output: PathBuf,
 
+    /// Source as LAZ tile DB
+    #[clap(long, group = "exclusive")]
+    pub laz_tile_db: Option<PathBuf>,
+
+    /// Source as LAZ index DB referring *.laz files
+    #[clap(long, group = "exclusive")]
+    pub laz_index_db: Option<PathBuf>,
+
+    /// EPSG:3857 bounding box to render
+    #[clap(long)]
+    pub bbox: BBox,
+
+    /// Projection of points if reading from *.laz; default is EPSG:3857
+    #[clap(long, conflicts_with = "laz_tile_db")]
+    pub source_projection: Option<String>,
+
     /// Max zoom level of tiles to generate
     #[clap(long)]
     pub zoom_level: u8,
+
+    /// If LAZ tile DB is used then use value of `--zoom-level` argument of `laztile`
+    /// If LAZ index is used then use zoom level to determine size of tile to process at once.
+    #[clap(long, default_value_t = 16)]
+    pub unit_zoom_level: u8,
 
     /// Shadings; `+` separated componets of shading. Shading component is <method>,method_param1[,method_param2...].
     /// ‎
@@ -27,14 +48,17 @@ pub struct Options {
     #[clap(long, verbatim_doc_comment)]
     pub shadings: Shadings,
 
+    /// Increase (> 1.0) or decrease (< 1.0) contrast of the shading. Use value higher than 0.0.
+    #[clap(long, default_value_t = 1.0)]
+    pub contrast: f64,
+
+    /// Increase (> 0.0) or decrease (< 0.0) brightness of the shading. Use value between -1.0 and 1.0.
+    #[clap(long, default_value_t = 0.0)]
+    pub brightness: f64,
+
     /// Z-factor
     #[clap(long, default_value_t = 1.0)]
     pub z_factor: f64,
-
-    /// If LAZ tile DB is used then use value of `--zoom-level` argument of `laztile`
-    /// If LAZ index is used then use zoom level to determine size of tile to process at once.
-    #[clap(long, default_value_t = 16)]
-    pub unit_zoom_level: u8,
 
     /// Tile size
     #[clap(long, default_value_t = 256)]
@@ -44,41 +68,17 @@ pub struct Options {
     #[clap(long, default_value_t = 40)]
     pub buffer: u32,
 
-    /// Source as LAZ tile DB
-    #[clap(long, group = "exclusive")]
-    pub laz_tile_db: Option<PathBuf>,
-
-    /// Source as LAZ index DB referring *.laz files
-    #[clap(long, group = "exclusive")]
-    pub laz_index_db: Option<PathBuf>,
-
-    /// Projection of points if reading from *.laz; default is EPSG:3857
-    #[clap(long, conflicts_with = "laz_tile_db")]
-    pub source_projection: Option<String>,
-
-    /// EPSG:3857 bounding box to render
-    #[clap(long)]
-    pub bbox: BBox,
-
-    /// Increase (> 1.0) or decrease (< 1.0) contrast. Use value higher than 0.0.
-    #[clap(long, default_value_t = 1.0)]
-    pub contrast: f64,
-
-    /// Increase (> 0.0) or decrease (< 0.0) brightness. Use value between -1.0 and 1.0.
-    #[clap(long, default_value_t = 0.0)]
-    pub brightness: f64,
-
-    /// Background color when writing to JPEG
-    #[clap(long, default_value = "FFFFFF")]
-    pub background_color: Rgb,
+    /// Tile image format. For alpha (transparency) support use `png`.
+    #[clap(long, value_enum, default_value_t = Format::JPEG)]
+    pub format: Format,
 
     /// Quality from 0 to 100 when writing to JPEG
     #[clap(long, default_value_t = 80)]
     pub jpeg_quality: u8,
 
-    /// Tile image format. For alpha (transparency) support use `png`.
-    #[clap(long, value_enum, default_value_t = Format::JPEG)]
-    pub format: Format,
+    /// Background color when writing to JPEG as it does not support alpha
+    #[clap(long, default_value = "FFFFFF")]
+    pub background_color: Rgb,
 }
 
 impl Options {
